f5e7df417f9198bee605ca0a3882907fc13079a8
Alesia <alesia.krotsikava@logic-way.com>
2016-06-15 13:14:05 +0300

    added chart.js

---
+++ js/Chart.js
@@ -0,0 +1,107 @@
+function MarkChart(canvasElementId) {
+    var canvas = document.getElementById(canvasElementId);
+    this.ctx = canvas.getContext('2d');
+    this.width = this.ctx.canvas.width;
+    this.height = this.ctx.canvas.height;
+
+    this.marginTop = 10;
+    this.marginBottom = 10;
+    this.marginLeft = 10;
+    this.marginRight = 10;
+
+    this.labelMargin = 10;
+    this.dataValueMargin = 2;
+
+    this.data = new Array();
+    this.colors = new Array();
+
+    this.StrokeStyle = '#fff';
+    this.BorderWidth = 2.0;
+
+    this.Start = 75;
+    this.Total = null;
+
+    this.draw = function () {
+        var context = this.ctx;
+        context.lineCap = 'round';
+        var minFactor = Math.min(this.widthSizeFactor, this.heightSizeFactor);
+
+        this.drawChart(true);
+    }
+
+    this.drawChart = function () {
+        var context = this.ctx;
+        context.lineWidth = this.BorderWidth;
+
+        var dataSum = 0,
+            dataSumForStartAngle = 0,
+            dataLen = this.data.length;
+
+        for (var i = 0; i < dataLen; i++) {
+            dataSumForStartAngle += this.data[i];
+            if (this.data[i] < 0) {
+                return;
+            }
+        }
+        if (this.Total == null) {
+            dataSum = dataSumForStartAngle;
+        } else {
+            dataSum = this.Total;
+        }
+
+        var AreaWidth = this.width - this.marginLeft - this.marginRight;
+        var AreaHeight = this.height - this.marginTop - this.marginBottom;
+
+        var centerX = this.width / 2;
+        var centerY = this.marginTop + (AreaHeight / 2);
+
+        var doublePI = 2 * Math.PI;
+        var radius = (Math.min(AreaWidth, AreaHeight) / 2);
+
+        var maxLabelWidth = 0;
+
+        radius = radius - maxLabelWidth - this.labelMargin;
+
+        var startAngle = this.Start * doublePI / dataSumForStartAngle;
+        var currentAngle = startAngle;
+        var endAngle = 0;
+        var incAngleBy = 0;
+
+        for (var i = 0; i < dataLen; i++) {
+            context.beginPath();
+            incAngleBy = this.data[i] * doublePI / dataSum;
+            endAngle = currentAngle + incAngleBy;
+
+
+            context.moveTo(centerX, centerY);
+            context.arc(centerX, centerY, radius, currentAngle, endAngle, false);
+            context.lineTo(centerX, centerY);
+
+            currentAngle = endAngle;
+
+            if (this.colors[i]) {
+                context.fillStyle = this.colors[i];
+            } else {
+                context.fillStyle = this.FillStyle;
+            }
+            context.fill();
+
+            context.strokeStyle = this.StrokeStyle;
+            context.stroke();
+        }
+
+        var ringCenterRadius = radius / 2;
+
+        context.save();
+
+        context.beginPath();
+        context.moveTo(centerX + ringCenterRadius, centerY);
+        context.arc(centerX, centerY, ringCenterRadius, 0, doublePI, false);
+
+        context.globalCompositeOperation = 'destination-out';
+        context.fillStyle = '#000';
+        context.fill();
+
+        context.restore();
+    }
+}
---
+++ js/yMap.js
@@ -0,0 +1,48 @@
+var yMapApp = angular.module("YMap", [ "kendo.directives" ])
+
+    .controller("CharactCtrl", function($scope){
+
+    })
+
+    .controller("MarkCtrl", function($scope){
+
+     	var myMap,
+     		myPlacemark;
+
+     	function init() {
+        	$scope.countOnMark = 1;
+        	console.log("Создание карты!!!")
+			var map = new ymaps.Map("map_container", {
+		        center: [55.73, 37.58],
+		        zoom: 10
+	    	});
+	    	
+	    	var polygonLayout = ymaps.templateLayoutFactory.createClass('<div class="placemark_layout_container"><div class="polygon_layout"><canvas id="chartCanvas" width="80%" height="80%"><span>{{ properties.chartCount }}</span></canvas></div></div>');
+
+			polygonPlacemark = new ymaps.Placemark(
+		        [55.66, 37.55], {
+		            hintContent: 'Метка',
+		            name: 'my name',
+		            chartCount: $scope.countOnMark
+		        }, {
+		            iconLayout: polygonLayout,
+		            iconShape: {   
+		                type: 'Polygon',
+		                coordinates: [
+		                    [[-28,-76],[28,-76],[28,-20],[12,-20],[0,-4],[-12,-20],[-28,-20]]
+		                ]
+		            }
+		        }
+		    );
+
+		    map.geoObjects.add(polygonPlacemark);
+
+		};
+
+		$scope.update = function() {
+			polygonPlacemark.properties.set("chartCount",$scope.countOnMark)
+		};
+		ymaps.ready(init);
+    });
+
+
