913a2aa5d2f0eec9c3c9c608d38bcda409cfd8ba
Alesia <alesia.krotsikava@logic-way.com>
2016-06-17 17:20:27 +0300

    Corrected window of features

--- js/yMap.js
+++ js/yMap.js
@@ -150,47 +150,48 @@ var yMapApp = angular.module("YMap", [ "kendo.directives" ])
 							'<div class="features-window" id="window">'+
 								'<div class="list-section k-content">'+
 									'<ul class="fieldlist">'+
-										'<li><input type="radio" name="colour-mark" id="colourm1" class="k-radio" checked="checked">'+
+										'<li><input type="radio" name="colour-mark" id="colourm1" value="Класс дома" class="k-radio" checked="checked">'+
 											'<label class="k-radio-label" for="colourm1">Класс дома</label></li>'+
-										'<li><input type="radio" name="colour-mark" id="colourm2" class="k-radio">'+
+										'<li><input type="radio" name="colour-mark" id="colourm2" value="Материал стен дома" class="k-radio">'+
 											'<label class="k-radio-label" for="colourm2">Материал стен дома</label></li>'+
-										'<li><input type="radio" name="colour-mark" id="colourm3" class="k-radio">'+
+										'<li><input type="radio" name="colour-mark" id="colourm3" value="Максимальная этажность  дома" class="k-radio">'+
 											'<label class="k-radio-label" for="colourm3">Максимальная этажность  дома</label></li>'+
-										'<li><input type="radio" name="colour-mark" id="colourm4" class="k-radio">'+
+										'<li><input type="radio" name="colour-mark" id="colourm4" value="Год постройки дома" class="k-radio">'+
 											'<label class="k-radio-label" for="colourm4">Год постройки дома</label></li>'+
-										'<li><input type="radio" name="colour-mark" id="colourm5" class="k-radio">'+
+										'<li><input type="radio" name="colour-mark" id="colourm5" value="Когорта дома Объекта" class="k-radio">'+
 											'<label class="k-radio-label" for="colourm5">Когорта дома Объекта </label></li>'+
 									'</ul>'+
 									'<ul class="fieldlist">'+
-										'<li><input type="radio" name="number-mark" id="numberm1" class="k-radio" checked="checked">'+
+										'<li><input type="radio" name="number-mark" id="numberm1" value="Количество Аналогов/Объявлений в доме" class="k-radio" checked="checked">'+
 											'<label class="k-radio-label" for="numberm1">Количество Аналогов/Объявлений в доме</label></li>'+
-										'<li><input type="radio" name="number-mark" id="numberm2" class="k-radio">'+
+										'<li><input type="radio" name="number-mark" id="numberm2" value="Максимальная этажность  дома" class="k-radio">'+
 											'<label class="k-radio-label" for="numberm2">Максимальная этажность  дома</label></li>'+
-										'<li><input type="radio" name="number-mark" id="numberm3" class="k-radio">'+
+										'<li><input type="radio" name="number-mark" id="numberm3" value="Год постройки дома" class="k-radio">'+
 											'<label class="k-radio-label" for="numberm3">Год постройки дома</label></li>'+
-										'<li><input type="radio" name="number-mark" id="numberm4" class="k-radio">'+
+										'<li><input type="radio" name="number-mark" id="numberm4" value="Количество описанных квартир в доме по Росреестру" class="k-radio">'+
 											'<label class="k-radio-label" for="numberm4">Количество описанных квартир в доме по Росреестру</label></li>'+
-										'<li><input type="radio" name="number-mark" id="numberm5" class="k-radio">'+
+										'<li><input type="radio" name="number-mark" id="numberm5" value="Средняя удельная цена Аналогов/Объявлений (тыс.руб.)" class="k-radio">'+
 											'<label class="k-radio-label" for="numberm5">Средняя удельная цена Аналогов/Объявлений (тыс.руб.)</label></li>'+
-										'<li><input type="radio" name="number-mark" id="numberm6" class="k-radio">'+
+										'<li><input type="radio" name="number-mark" id="numberm6" value="Удельная Кадастровая стоимость (тыс.руб.)" class="k-radio">'+
 											'<label class="k-radio-label" for="numberm6">Удельная Кадастровая стоимость (тыс.руб.)</label></li>'+
 									'</ul>'+
 									'<ul class="fieldlist">'+
-										'<li><input type="radio" name="diagram-mark" id="diagramm1" class="k-radio" checked="checked">'+
+										'<li><input type="radio" name="diagram-mark" id="diagramm1" value="Доли Аналогов/Объявлений в доме по комнатности (1, 2, 3 , 4+)" class="k-radio" checked="checked">'+
 											'<label class="k-radio-label" for="diagramm1">Доли Аналогов/Объявлений в доме по комнатности (1, 2, 3 , 4+)</label></li>'+
-										'<li><input type="radio" name="diagram-mark" id="diagramm2" class="k-radio">'+
+										'<li><input type="radio" name="diagram-mark" id="diagramm2" value="Доли Аналогов/Объявлений в доме по классу (дихотомия: доминирующий класс и остальные)" class="k-radio">'+
 											'<label class="k-radio-label" for="diagramm2">Доли Аналогов/Объявлений в доме по классу (дихотомия: доминирующий класс и остальные)</label></li>'+
 									'</ul>'+
 									'<ul class="fieldlist">'+
-										'<li><input type="radio" name="special-mark" id="specialm1" class="k-radio" checked="checked">'+
+										'<li><input type="radio" name="special-mark" id="specialm1" value="В доме выданы кредиты: да/нет (информация о выданных кредитах в доме)" class="k-radio" checked="checked">'+
 											'<label class="k-radio-label" for="specialm1">В доме выданы кредиты: да/нет (информация о выданных кредитах в доме)</label></li>'+
-										'<li><input type="radio" name="special-mark" id="specialm2" class="k-radio">'+
+										'<li><input type="radio" name="special-mark" id="specialm2" value="Описание дома рассогласовано: да/нет (характеристики рассогласованности)" class="k-radio">'+
 											'<label class="k-radio-label" for="specialm2">Описание дома рассогласовано: да/нет (характеристики рассогласованности)</label></li>'+
 									'</ul>'+
 					        '</div>'+
-							'<button class="k-button controlFeatures" id="resetFeatures">Отменить</button>'+
+							'<button class="k-button controlFeatures" id="refreshFeatures">Отменить</button>'+
 							'<button class="k-button controlFeatures" id="sendFeatures">Показать</button>'+
 					'</div>').appendTo(parentDomContainer);
+					this._mapEventGroup = this.getMap().events.group();
 
 					var myWindow = $("#window"),
 						features = $("#features");
@@ -200,7 +201,36 @@ var yMapApp = angular.module("YMap", [ "kendo.directives" ])
 						features.fadeOut();
 					});
 
+					$('#resetFeatures').click(function() {
+							myWindow.data("kendoWindow").refresh();
+					});
+
+					$("body").click(function(){
+							myWindow.data("kendoWindow").refresh();
+					});
+
+					$('#sendFeatures').click(function(){
+						var vals = $('.k-radio').map(function(i,el){
+							if($(el).prop('checked')){
+								return $(el).val();
+							}
+						}).get();
+						/*$.ajax(
+								{
+									type: "POST",
+									url: "/",
+									dataType: "json",
+									data: {chArr: vals},
+									cashe: false,
+									success: function(data){
+									}
+								});
+						return false;*/
+						console.log(vals);
+					});
+
 					function onClose() {
+						myWindow.data("kendoWindow").refresh();
 						features.fadeIn();
 					}
 
@@ -211,9 +241,8 @@ var yMapApp = angular.module("YMap", [ "kendo.directives" ])
 							"Close"
 						],
 						close: onClose
-					}).data("kendoWindow").center();
-
-				}
+					});
+					}
 
 				/*_createRequest: function() {
 					var lastCenter = this._lastCenter = this.getMap().getCenter().join(',');
